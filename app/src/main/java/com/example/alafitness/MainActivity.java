9732f7844b65cef3e96cb003cb553130c7bfed25
Laila Duffy <lail.duffy@gmail.com>
2021-12-29 17:29:13 +0000

    Created database for username and password storage using SQLite, next will need to figure out how not to have the sign up page show up everytime

--- app/src/main/java/com/example/alafitness/MainActivity.java
+++ app/src/main/java/com/example/alafitness/MainActivity.java
@@ -2,13 +2,62 @@ package com.example.alafitness;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText username, password, retypePassword;
+    Button buttonSignUp;
+    DBHelper DB;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sing_up);
+
+        username = findViewById(R.id.new_username);
+        password = findViewById(R.id.new_password);
+        retypePassword = findViewById(R.id.retype_password);
+        buttonSignUp = findViewById(R.id.sign_up_button);
+        DB = new DBHelper(this);
+
+        buttonSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+                String repass = retypePassword.getText().toString();
+
+                if (user.equals("") || pass.equals("") || repass.equals("")) {
+                    Toast.makeText(MainActivity.this, "Please enter your username, the new password and retype the new password!", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (pass.equals(repass)) {
+                        Boolean checkUser = DB.checkUsername(user);
+                        if (checkUser == false) {
+                            Boolean insert = DB.insertData(user, pass);
+                            if (insert == true) {
+                                Toast.makeText(MainActivity.this, "User successfully registered!", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(), profile.class);
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(MainActivity.this, "Registration has failed!", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(MainActivity.this, "User already exists! Please sign in", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(MainActivity.this, "Passwords do not match! Please check!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+            });
+        }
+
+
     }
-}
