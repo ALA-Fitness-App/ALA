b5dadf9af7ab0fa850d72a0e262a28afdea8c33e
Laila Duffy <lail.duffy@gmail.com>
2022-01-07 20:02:25 +0000

    Reverted to Aijas commit, added express button to get to the exercise page from the profile

--- app/src/main/java/com/example/alafitness/profile.java
+++ app/src/main/java/com/example/alafitness/profile.java
@@ -2,25 +2,30 @@ package com.example.alafitness;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class profile extends AppCompatActivity {
 
-    /*expressButton = (Button) findViewById(R.id.imageButton2);
+    ImageButton expressButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        expressButton = findViewById(R.id.imageButton2);
 
         expressButton.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Intent intent2 = new Intent(profile.this,GetReady.class);
-            startActivity(intent2);
-        }
-    }*/
-
-    public void onClick(View view) {
-        Intent intent2 = new Intent(profile.this,GetReady.class);
-        startActivity(intent2);
+            @Override
+            public void onClick(View view) {
+                Intent intent2 = new Intent(profile.this,ExerciseActivity.class);
+                startActivity(intent2);
+            }
+        });
     }
-    }
+}
