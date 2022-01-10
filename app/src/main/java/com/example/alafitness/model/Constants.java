cbe1a894e474e4432102f3b4f858abf8db5743b0
annab <annabo@inbox.lv>
2022-01-10 23:10:01 +0200

    Exercise functionality implemented.

--- app/src/main/java/com/example/alafitness/model/Constants.java
+++ app/src/main/java/com/example/alafitness/model/Constants.java
@@ -11,11 +11,11 @@ public class Constants {
 
         ArrayList<Exercise> exercisesList = new ArrayList<Exercise>();
 
-        exercisesList.add(new Exercise("Plank", "Core", R.drawable.plank, 10_000));
-        exercisesList.add(new Exercise("Break", "Break", R.drawable.workout_end, 10_000));
-        exercisesList.add(new Exercise("Squat", "Legs", R.drawable.squat, 10_000));
-        exercisesList.add(new Exercise("Break", "Break", R.drawable.workout_end, 10_000));
-        exercisesList.add(new Exercise("Push up", "Arms", R.drawable.pushup, 10_000));
+        exercisesList.add(new Exercise("Plank", "Core", R.drawable.plank, 10l));
+        exercisesList.add(new Exercise("Break", "Break", R.drawable.workout_end, 10l));
+        exercisesList.add(new Exercise("Squat", "Legs", R.drawable.squat, 10l));
+        exercisesList.add(new Exercise("Break", "Break", R.drawable.workout_end, 10l));
+        exercisesList.add(new Exercise("Push up", "Arms", R.drawable.pushup, 10l));
 
         return exercisesList;
     }
